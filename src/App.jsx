4635a48a11866133bf811adeee017d1b36997680
Carlos Bonilla <bonillagalindocarlos@gmail.com>
2023-03-27 13:24:37 -0600

    Seguimientod de pacietes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-
+import Header from './components/Header'
+import ListadoPacientes from './components/ListadoPacientes'
+import Formulario from './components/Formulario'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+    <div className='container mx-auto mt-20'>
+      <Header/>
+      <div className='mt-12 md:flex'>
+        <Formulario/>
+        <ListadoPacientes/>
+      </div>
     </div>
   )
 }
